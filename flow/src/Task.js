6f19dcdc0b779bf8d4ad9983a8ffe43d3d6f220a
Adam Allison <allisonadam81@gmail.com>
2025-10-22 21:54:35 -0700

    move stuff

--- flow/src/Task.js
+++ flow/src/Task.js
@@ -3,16 +3,23 @@ class Task {
     this.work = work;
   }
 
-  static of = (fn) => new Task(fn);
+  static of = (fn) => {
+    if (typeof fn === 'function') return new Task(fn);
+    return new Task(async () => fn);
+  };
 
   map = (fn) => Task.of(async () => fn(await this.work()));
 
-  flatMap = (fn) => async () => fn(await this.work());
+  flatMap = (fn) =>
+    Task.of(async () => {
+      const nextTask = fn(await this.work());
+      return nextTask.run();
+    });
 
   catch = (fn) =>
     Task.of(async () => {
       try {
-        return this.work();
+        return await this.work();
       } catch (e) {
         return fn(e);
       }
