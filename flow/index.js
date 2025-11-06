121b6c02a3794bcb3502e192eea9c89486cff29d
Adam Allison <allisonadam81@gmail.com>
2025-11-06 22:37:45 +0000

    added more methods

--- flow/index.js
+++ flow/index.js
@@ -1,1 +1,48 @@
-export { FlowBox } from './classes/FlowBox'
+// CLASSES
+export { FlowBox } from './classes/FlowBox.js';
+export { EagerFlowBox } from './classes/EagerFlowBox.js';
+
+// DEBUG
+export { trace } from './functions/debug/trace.js';
+export { traceWithCxt } from './functions/debug/traceWithCxt.js';
+
+// IS
+export { isArray } from './functions/is/isArray.js';
+export { isBool } from './functions/is/isBool.js';
+export { isError } from './functions/is/isError.js';
+export { isFunction } from './functions/is/isFunction.js';
+export { isNull } from './functions/is/isNull.js';
+export { isNullOrUnd } from './functions/is/isNullOrUnd.js';
+export { isNumber } from './functions/is/isNumber.js';
+export { isObject } from './functions/is/isObject.js';
+export { isPromise } from './functions/is/isPromise.js';
+export { isString } from './functions/is/isString.js';
+export { isUndefined } from './functions/is/isUndefined.js';
+
+// ITERATORS
+export { compose } from './functions/iterators/compose.js';
+export { composeWithCxt } from './functions/iterators/composeWithCxt.js';
+export { filter } from './functions/iterators/filter.js';
+export { filterTruthy } from './functions/iterators/filterTruthy.js';
+export { flatMap } from './functions/iterators/flatMap.js';
+export { map } from './functions/iterators/map.js';
+export { pipe } from './functions/iterators/pipe.js';
+export { pipeWithCxt } from './functions/iterators/pipeWithCxt.js';
+
+// LOCATORS'
+export { head } from './functions/locators/head.js';
+export { identity } from './functions/locators/identity.js';
+export { prop } from './functions/locators/prop.js';
+export { tail } from './functions/locators/tail.js';
+
+// MISC
+export { cheapClone } from './functions/misc/cheapClone.js';
+export { deferToMacroQueue } from './functions/misc/deferToMacroQueue.js';
+export { deferToMicroQueue } from './functions/misc/deferToMicroQueue.js';
+export { rethrow } from './functions/misc/rethrow.js';
+export { safeParse } from './functions/misc/safeParse.js';
+export { sleep } from './functions/misc/sleep.js';
+
+// TO
+export { toArray } from './functions/to/toArray.js';
+export { toString } from './functions/to/toString.js';
